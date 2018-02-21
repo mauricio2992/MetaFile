9c4a92fc8ca0825040f1cb984f05c30768930b5f
maurcio2992 <mauricio.chaves.92@gmail.com>
2018-02-21 00:06:06 -0500

    metafile

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MetaFile
+
+MetaFile es un script creado por mauricio2992 en Python para extracción de los metadatos de los archivos PDF y JPG de un directorio. Para su funcionamiento es necesaria la instalación de las librerías PyPDF2 y pyexiv2, ejecutando en Debian:
+
+$sudo apt-get install python-pyexiv2 python-pyexiv2-doc
+$sudo pip install PyPDF2
+
+para su uso se ejecuta:
+
+python metafile.py [option] [path]
+
+Options: 1. PDF
+         2. JPG
+
+Ejemplo:
+python metafile.py 1 /root/Documents 
---
+++ metafile.py
@@ -0,0 +1,60 @@
+# -*- encoding: utf-8 -*-
+# AUTOR: mauricio2992
+
+
+from PyPDF2 import PdfFileReader, PdfFileWriter #importamos modulo y librerias
+import os #importamos modulo socket para ir a otras carpetas
+import sys
+import pyexiv2 #importamos modulo para informacion exif, se debe ejecutar apt-get install python-pyexiv2 python-pyexiv2-doc
+
+print chr(27)+"[0;92m"
+print " /$$      /$$             /$$                     /$$$$$$$$ /$$ /$$          "
+print "| $$$    /$$$            | $$                    | $$_____/|__/| $$          "
+print "| $$$$  /$$$$  /$$$$$$  /$$$$$$    /$$$$$$       | $$       /$$| $$  /$$$$$$ "
+print "| $$ $$/$$ $$ /$$__  $$|_  $$_/   |____  $$      | $$$$$   | $$| $$ /$$__  $$"
+print "| $$  $$$| $$| $$$$$$$$  | $$      /$$$$$$$      | $$__/   | $$| $$| $$$$$$$$"
+print "| $$\  $ | $$| $$_____/  | $$ /$$ /$$__  $$      | $$      | $$| $$| $$_____/"
+print "| $$ \/  | $$|  $$$$$$$  |  $$$$/|  $$$$$$$      | $$      | $$| $$|  $$$$$$$"
+print "|__/     |__/ \_______/   \___/   \_______/      |__/      |__/|__/ \_______/"
+print "                                                                             "
+
+print chr(27)+"[0m"
+
+def printMeta(ruta): # funcion que obtiene los metadatos de archivos pdf en un directorio
+	for dirpath, dirnames, files in os.walk(ruta): # para el diretorio, nombre y archivos en la carpeta docs
+		for name in files: #recorremos los posibles fichreos
+			ext = name.lower().rsplit('.', 1)[-1]
+			if ext in ['pdf']:
+				print chr(27)+"[0;31m"+"[+] Metadata for file: %s " %(dirpath+os.path.sep+name)+chr(27)+"[0m" # pintamos el titulo de metadata for file y el directorio y nombre del documento
+				pdfFile = PdfFileReader(file(dirpath+os.path.sep+name, 'rb')) # abrimos el fichero
+				docInfo = pdfFile.getDocumentInfo() # creamos un diccionario con la info recolectada
+				for metaItem in docInfo:
+					print '[+] ' + metaItem + ':' + docInfo[metaItem]
+				print "\n"
+
+def metaJpg(ruta): # funcion que obtiene los metadatos exif de una imagen jpg
+	for dirpath,dirname,files in os.walk(ruta): 
+		for name in files:
+			ext=name.lower().rsplit('.',1)[-1]
+			if ext in ['jpg']:
+				metadata = pyexiv2.ImageMetadata(dirpath+os.path.sep+name)
+				metadata.read()
+				print chr(27)+"[0;31m"+"### Metadata for file: %s" %(dirpath+os.path.sep+name)+chr(27)+"[0m"
+				color=2 # variable para variar de color cada linea
+				for metadato in metadata.exif_keys: 
+					if color==8: # vuelve e inicia en el color 2, verde
+						color=2
+					texto = metadato + ": " + metadata[metadato].raw_value # almacena la informacion y la imprime en colores
+					print chr(27)+"[0;3%dm" % color+texto
+					color=color+1
+				print "\n"+chr(27)+"[0m"
+
+if len(sys.argv)==3:
+	if sys.argv[1]=="1":
+		printMeta(sys.argv[2]) # invocamos la funcion con la ruta como parametro '''
+	elif sys.argv[1]=="2":
+		metaJpg(sys.argv[2]) # invocamos la funcion con la ruta como parametro
+	else:
+		print "Options \n\n1. PDF\n2. JPG\n\nUse:\npython metadatos.py [option] [path]\n\nExample:\npython metadatos.py 1 /root/Escritorio"
+else:
+	print "Options \n\n1. PDF\n2. JPG\n\nUse:\npython metadatos.py [option] [path]\n\nExample:\npython metadatos.py 1 /root/Escritorio"
